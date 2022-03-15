8cd7a167eaa89303d438cb847e7c61126e3c10d2
Ryan Pierre <info@ryanpierre.com>
2022-03-15 22:20:24 +0000

    finish withdrawal and balance features

--- src/main/scala/Account.scala
+++ src/main/scala/Account.scala
@@ -8,10 +8,22 @@ class Account(
 ) {
 
   def balance(): Double = {
-    transactions.map(_.amount).reduceLeft(_ + _)
+    transactions.map(_.amount).foldLeft(0.0)(_ + _)
   }
 
   def deposit(amount: Double): Unit = {
+    addTransaction(amount)
+  }
+
+  def withdraw(amount: Double): Unit = {
+    if(amount > balance()) {
+      throw new RuntimeException("Not enough money!")
+    }
+
+    addTransaction(-1 * amount)
+  }
+
+  private def addTransaction(amount: Double): Unit = {
     transactions = transactions :+ transactionService.createTransaction(amount)
   }
 }
--- src/test/scala/AccountTest.scala
+++ src/test/scala/AccountTest.scala
@@ -7,20 +7,43 @@ class AccountTest extends AnyWordSpec with Matchers with MockFactory {
   "An Account" should {
     "accepts a deposit" which {
       "stores a new transaction" in {
-        // Set up mocks
         val mockTransactionService = mock[BaseTransactionService]
-        val mockTransaction = mock[BaseTransaction]
+        val mockDeposit = mock[BaseTransaction]
+
         (mockTransactionService.createTransaction _)
           .expects(100)
-          .returning(mockTransaction)
+          .returning(mockDeposit)
 
-        // Set up test
         val subject = new Account(Seq(), mockTransactionService);
-        subject.deposit(100)
 
-        // Assert !
+        subject.deposit(100)
         subject.transactions should have length 1
-        subject.transactions(0) should equal(mockTransaction)
+        subject.transactions(0) shouldEqual mockDeposit
+      }
+    }
+    "accept a withdrawal" which {
+      "stores a new transaction" in {
+        val mockTransactionService = mock[BaseTransactionService]
+        val mockDeposit = mock[BaseTransaction]
+        val mockWithdrawal = mock[BaseTransaction]
+
+        (mockTransactionService.createTransaction _)
+          .expects(-50)
+          .returning(mockWithdrawal)
+
+        val subject = new Account(Seq(mockDeposit), mockTransactionService) {
+          override def balance(): Double = 100.0
+        };
+
+        subject.withdraw(50)
+        subject.transactions should have length 2
+        subject.transactions(1) shouldEqual mockWithdrawal
+      }
+      "throws an error if the requested amount is more than balance" in {
+        val mockTransactionService = mock[BaseTransactionService]
+        val subject = new Account(Seq(), mockTransactionService)
+
+        a[RuntimeException] should be thrownBy subject.withdraw(50)
       }
     }
     "calculate balance" which {
@@ -33,9 +56,10 @@ class AccountTest extends AnyWordSpec with Matchers with MockFactory {
         (mockTransaction2.amount _).expects().returning(-50.5)
         (mockTransaction3.amount _).expects().returning(200)
 
-        val subject = new Account(Seq(mockTransaction1, mockTransaction2, mockTransaction3))
+        val subject =
+          new Account(Seq(mockTransaction1, mockTransaction2, mockTransaction3))
 
-        subject.balance should equal(250)
+        subject.balance shouldEqual 250
       }
     }
   }
